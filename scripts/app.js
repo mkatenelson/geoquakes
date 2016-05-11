99e7ca780c51939e2c40416f30856cbc250cad0d
Kate Nelson <mkatenelson@gmail.com>
2016-05-11 15:11:40 -0700

    Part 1 in-class

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,24 @@ var $quakesList;
 var map;
 var template;
 
-$(document).on("ready", function() {
+$(document).on("ready", function(){
 
+    $quakesList = $.ajax({
+        method: "GET",
+        url: weekly_quakes_endpoint
+    })
+    .done(function(data){
 
+        var earthquake = data.features;
+        
+        var source = $("#quakes-template").html();
+        template = Handlebars.compile(source);
+
+        var quakesTemplate = template({quakes: earthquake});
+
+        $("#info").append(quakesTemplate);
+    })
+    .fail(function(response){
+        console.log("Error", response);
+    })
 });
